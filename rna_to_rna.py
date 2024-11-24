8f767a195e03c39eca76031222cf22341626026f
jhwptsd <jhwptsd@gmail.com>
2024-11-24 11:16:19 -0500

    Fixed rna_to_rna.py

--- rna_to_rna.py
+++ rna_to_rna.py
@@ -1,9 +1,9 @@
-from parse_RNAs import parse_RNA
+from parse_RNAs import parse_rna
 from loss import RMSD, tm_score
 
 def rna_to_rna(generated, template_path, tm=False):
     gen_points, _ = generated.data()
-    temp_points, _ = parse_RNA(template_path)
+    temp_points, _ = parse_rna(template_path)
     if tm:
         return tm_score(gen_points, temp_points)
     else:
