f04a5650967b28bfc650af1f466e8e42b08ba975
jhwptsd <jhwptsd@gmail.com>
2024-11-24 15:12:40 -0500

    Revert "Update precompute.py"
    This reverts commit d0565640578fa6d734a00418454fe93639a9f14d.

--- precompute.py
+++ precompute.py
@@ -3,7 +3,7 @@ import os
 
 def make_dir(seqs):
     os.makedirs("FASTAs", exist_ok=True)
-    for tag, seq in list(seqs.items()):
+    for tag, seq in enumerate(seqs):
         with open(f"FASTAs/{tag}.fasta", "w") as f:
             f.write(f">{tag}\n{seq}")
             
